fdd6a5fc788e35a63af2be66688becee3bb8d418
Yuki.N <Syukkic@Gmail.com>
2022-11-19 21:30:07 +0800

    cmp finally works:)

--- neovim/lua/plugins/cmp.lua
+++ neovim/lua/plugins/cmp.lua
@@ -1,44 +1,37 @@
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-    return
+  return
+end
+
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+  return
 end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_backspace = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[-1]:sub(col, col):match "%s" == nil
-end
+vim.opt.completeopt = "menu,menuone,noselect"
+
 
 cmp.setup {
     snippet = {
         expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
+            luasnip.lsp_expand(args.body)
         end,
     },
-    window = {
-    },
     mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c"}),
-        ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c"}),
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c"}),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c"}),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c"}),
-        ["<m-o>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-
-        ["Tab"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif luasnip.expandable() then
-                luasnip.expand()
-            elseif check_backspace() then
-                fallback()
-            else
-                fallback()
-            end
-        end
-        ),
-    })
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    }),
+    sources = cmp.config.sources({
+        { name = "luasnip"}, -- snippets
+        { name = "buffer" }, -- text within current buffer
+        { name = "path"},    -- file system paths
+    }),
 }
+
