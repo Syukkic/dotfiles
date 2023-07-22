08a1a1f3288afce5d49ca0031a6ae00a05dcfa36
Yuki.N <Syukkic@Gmail.com>
2023-07-22 23:45:21 +0800

    update scripts

--- scripts/rsync_local_backup.sh
+++ scripts/rsync_local_backup.sh
@@ -5,43 +5,54 @@ decrypted="driver"
 decrypted_point="/dev/mapper/driver"
 mounted_point="/mnt"
 need_to_backup="/home/yuki/"
+today=$(date +%Y%m%d)
 
-today=`date +%Y%m%d`
+start () {
+    lsblk
+    read -p "Choose the partition to be decrypted: " partition
 
-echo "================================"
-echo "|   lsblk display partitions   |"
-echo "================================"
+    encrypted_partition="/dev/${partition}"
 
-lsblk
-read -p "Choose the partition to being decrypted: " partition
+    if ! cryptsetup isLuks "${encrypted_partition}"; then
+        echo "ERROR: ${encrypted_partition} is not a LUKS partition."
+        exit 1
+    fi
 
-encrypted_partition="/dev/${partition}"
+    cryptsetup open "${encrypted_partition}" "${decrypted}"
+    echo "Found external driver --> ${encrypted_partition}"
 
-cryptsetup open ${encrypted_partition} ${decrypted}
-echo "Found external driver --> $encrypted_partition"
+    mount "${decrypted_point}" "${mounted_point}" || { echo "Failed to mount the external hard drive."; exit 1; }
+    echo "External hard drive is mounted"
 
-mount ${decrypted_point} ${mounted_point}
-echo "External hard drive is mounted"
+    if [[ -d "${mounted_point}/Backup" ]]; then
+        let n=1 
+        for dir in "${mounted_point}/Backup"/*
+        do
+            echo "$n: ${dir}"
+            n=$((n+1))
+        done
+    else
+        echo "Could not find external driver. It may not be mounted."
+    fi
 
-if [[ -d "${mounted_point}/Backup" ]]; then
-    let n=1 
-    for dir in ${mounted_point}/Backup/*
-    do echo $n: ${dir}
-        n=$((n+1))
-    done
-else
-    echo "Could not find external driver. It may not be mounted"
-fi
+    read -p "Do you want to delete backup data? (yes / no) " whether_delete
+    if [[ "$whether_delete" == "yes" ]]; then
+        read -p "Enter the directory to delete: " to_delete
+        echo "rm -rv ${mounted_point}/Backup/${to_delete}"
+        rm -rv "${mounted_point}/Backup/${to_delete}"
+        rsync -aviHAXKhS --one-file-system --partial --info=progress2 --atimes --open-noatime --exclude-from=exclusive.txt \
+            "${need_to_backup}" "${mounted_point}/Backup/${today}/"
+    else
+        rsync -aviHAXKhS --one-file-system --partial --info=progress2 --atimes --open-noatime --exclude-from=exclusive.txt \
+            "${need_to_backup}" "${mounted_point}/Backup/${today}/"
+    fi
+}
+    
+stop () {
+    umount -R ${mounted_point}
+    sudo cryptsetup close driver
+}
 
-echo "=============================================="
-echo "|   Please delele the last one backup data   |"
-echo "|   Please delele the last one backup data   |"
-echo "|   Please delele the last one backup data   |"
-echo "=============================================="
+start
+stop
 
-
-read -p "You will delete the directory is: " to_delete
-echo "rm -rv /mnt/Backup"/${to_delete}
-rm -rv "/mnt/Backup"/${to_delete}
-
-rsync -aAXHv --exclude-from=exclusive.txt ${need_to_backup} "${mounted_point}/Backup/${today}/"
