f8cb599c5587ed4b9003f0883d1c6c7c95f085a2
Yuki.N <Syukkic@Gmail.com>
2023-09-09 08:30:19 +0800

    regular update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,15 +4,15 @@ local config = {
     window_background_opacity = 0.9;
     font = wezterm.font_with_fallback {
         "JetBrains Mono",
-        "Fira Code Nerd Font",
-        "Cascadia Code",
-        "Nerd Font",
+        "Noto Color Emoji",
         "Source Han Sans SC",
         "Source Han Sans TC",
     };
     font_size = 10.5;
     enable_tab_bar = false;
     default_prog = { '/bin/zsh', '-l', '-c', 'tmux attach || tmux' };
+
+    warn_about_missing_glyphs = false,
 
     hyperlink_rules = wezterm.default_hyperlink_rules();
 }
