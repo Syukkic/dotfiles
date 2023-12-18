b811dedf2785a302bacad2511d7e0d7dfb4ce2f4
Yuki.N <Syukkic@Gmail.com>
2023-12-18 23:16:16 +0800

    updated wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,21 +1,23 @@
 local wezterm = require("wezterm")
 local config = {
-    color_scheme = "Gruvbox dark, medium (base16)";
-    window_background_opacity = 0.9;
-    font = wezterm.font_with_fallback {
-        "JetBrains Mono",
-        "Noto Color Emoji",
-        "Source Han Sans SC",
-        "Source Han Sans TC",
-    };
-    font_size = 10.5;
-    enable_tab_bar = false;
-    -- default_prog = { '/bin/zsh', '-l', '-c', 'tmux attach || tmux' };
+	-- color_scheme = "Gruvbox dark, medium (base16)";
+	color_scheme = "Solarized Dark Higher Contrast (Gogh)",
+	-- calt=0 means ligatures-disabled
+	harfbuzz_features = { "calt=0" },
+	window_background_opacity = 0.9,
+	font = wezterm.font_with_fallback({
+		"JetBrains Mono",
+		"Noto Color Emoji",
+		"Source Han Sans SC",
+		"Source Han Sans TC",
+	}),
+	font_size = 10.5,
+	enable_tab_bar = false,
+	-- default_prog = { "/bin/zsh", "-l", "-c", "tmux attach || tmux" },
 
-    warn_about_missing_glyphs = false,
+	warn_about_missing_glyphs = false,
 
-    hyperlink_rules = wezterm.default_hyperlink_rules();
+	hyperlink_rules = wezterm.default_hyperlink_rules(),
 }
-
 
 return config
