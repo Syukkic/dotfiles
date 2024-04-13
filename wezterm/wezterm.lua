75d8412ee02c5bbb9e8d3c6a9eaf67971ab96ff6
Yuki.N <Syukkic@gmail.com>
2024-04-13 21:35:42 +0800

    regularly updated

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,22 @@
 local wezterm = require("wezterm")
 local config = {
 	color_scheme = "Gruvbox dark, hard (base16)",
-	-- color_scheme = "Vs Code Dark+ (Gogh)",
 	-- calt=0 means ligatures-disabled
 	harfbuzz_features = { "calt=0" },
-	window_background_opacity = 0.9,
+	window_background_opacity = 1,
 	font = wezterm.font_with_fallback({
 		"JetBrains Mono",
+		"Noto Sans CJK HK",
+		"Noto Sans CJK SC",
+		"Noto Sans CJK JP",
 		"Noto Color Emoji",
-		"Source Han Sans SC",
-		"Source Han Sans TC",
 	}),
-	font_size = 10.5,
-	enable_tab_bar = false,
+	font_size = 10.0,
+	enable_tab_bar = true,
+	enable_wayland = true,
 	-- default_prog = { "/bin/zsh", "-l", "-c", "tmux attach || tmux" },
 
 	warn_about_missing_glyphs = false,
-
 	hyperlink_rules = wezterm.default_hyperlink_rules(),
 }
 
