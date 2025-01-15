ae6dfe24c9c35dac88037f0bf71d8dc2c71ff316
Yuki.N <Syukkic@gmail.com>
2025-01-16 00:48:03 +0800

    Add `--split-chapter`

--- shell/fish/functions/ytm.fish
+++ shell/fish/functions/ytm.fish
@@ -1,16 +1,34 @@
 # Download YouTube Music
-function ytm
+function ytm 
     if string match -q -- "*playlist*" $argv
-        # if [ $argv == *"playlist"* ]
-        yt-dlp -x -f "bestaudio[ext=m4a]" \
+        yt-dlp \
+            --extract-audio \
+            --format "bestaudio" \
+            --audio-format "mp3" \
             --add-metadata --embed-thumbnail \
             --parse-metadata "playlist_index:%(track_numbers)s" \
-            -o "~/Music/%(playlist_index)s %(title)s.%(ext)s" \
+            --output "~/Music/%(playlist_index)s-%(title)s.%(ext)s" \
+            $argv
+    else if string match -q -- "*split-chapter*" $argv
+        # e.g.  [ ytm --split-chapter "https://www.youtube.com/watch?v=xkDlet4cxuQ" ]
+        yt-dlp \
+            --extract-audio \
+            --format "bestaudio" \
+            --audio-format "mp3" \
+            #--paths temp:"/tmp" \
+            --paths "~/Music" \
+            --split-chapter \
+            #--add-metadata --embed-chapters \
+            --add-metadata --embed-metadata \
+            --output "chapter:%(section_number)s - %(section_title)s.%(ext)s" \
             $argv
     else
-        yt-dlp -x -f "bestaudio[ext=m4a]" \
+        yt-dlp \
+            --extract-audio \
+            --format "bestaudio" \
+            --audio-format "mp3" \
             --add-metadata --embed-thumbnail \
-            -o "~/Music/%(title)s.%(ext)s" \
+            --output "~/Music/%(title)s.%(ext)s" \
             $argv
     end
 end
