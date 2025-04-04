ce2f8981b4ce818155c6b588452a7bdc69d7e8af
Yuki.N <Syukkic@gmail.com>
2025-04-04 12:26:33 +0800

    remap key to stop searching

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,8 +7,8 @@ vim.keymap.set("n", "<C-s>", "<cmd>w<cr>")
 -- make missing : less annoying
 vim.keymap.set("n", ";", ":")
 -- Ctrl+h to stop searching
-vim.keymap.set("v", "<leader>Q", "<cmd>nohlsearch<cr>")
-vim.keymap.set("n", "<leader>Q", "<cmd>nohlsearch<cr>")
+vim.keymap.set("v", "<leader>-", "<cmd>nohlsearch<cr>")
+vim.keymap.set("n", "<leader>-", "<cmd>nohlsearch<cr>")
 -- Jump to start and end of line using the home row keys
 vim.keymap.set("", "H", "^")
 vim.keymap.set("", "L", "$")
